a598401f9814d197c72cc119455151cf46f431de
Ember P <ember-devops@gmail.com>
2021-05-30 15:09:49 -0500

    numerical birthday entered

---
+++ ageCalculator.py
@@ -0,0 +1,6 @@
+year = input('What year where you born?\n')
+month = input('Enter your numerical birth month (ex:January input: 1)\n')
+day = input('Enter your numerical birth date (ex: 4th input: 4)\n')
+print("you entered: " + str(month) + '/' + str(day) + '/' + str(year))
+input('Is this correct? (y/N) ')
+input()
