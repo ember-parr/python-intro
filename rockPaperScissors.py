5eb441ec4754bd45e191a6174d456dd92ee26c7f
Ember P <ember-devops@gmail.com>
2021-05-30 16:47:54 -0500

    added random choice to rockpaperscissors game

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,5 +1,17 @@
-computer_choice = "rock"
-user_choice = input("enter one --> rock, paper or scissors")
+import random
+
+gameChoices = ["Rock", "Paper", "Scissors"]
+computer_choice = random.choice(gameChoices)
+user_choice = input("enter one --> Rock, Paper or Scissors\n").capitalize()
+
 
 if computer_choice == user_choice:
-    print("Tie!")
+    print("TIE GAME!")
+elif user_choice == "Rock" and computer_choice == "Scissors":
+    print("Rock smashes scissors... you WIN!")
+elif user_choice == "Scissors" and computer_choice == "Paper":
+    print("Scissors cut paper... you WIN!")
+elif user_choice == "Paper" and computer_choice == "Rock":
+    print("Paper covers rock... you WIN!")
+else:
+    print("Computer chose " + computer_choice + " you LOST!")
