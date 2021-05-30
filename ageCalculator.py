cf422e69e03906945ff29b9ff502ae8254d7ada4
Ember P <ember@emberparr.com>
2021-05-30 18:12:16 -0500

    playing with datetime

--- ageCalculator.py
+++ ageCalculator.py
@@ -1,6 +1,22 @@
-year = input('What year where you born?\n')
-month = input('Enter your numerical birth month (ex:January input: 1)\n')
-day = input('Enter your numerical birth date (ex: 4th input: 4)\n')
-print("you entered: " + str(month) + '/' + str(day) + '/' + str(year))
-input('Is this correct? (y/N) ')
-input()
+import datetime
+
+# year = input('What year where you born?\n')
+# month = input('Enter your numerical birth month (ex:January input: 1)\n')
+# day = input('Enter your numerical birth date (ex: 4th input: 4)\n')
+# print("you entered: " + str(month) + '/' + str(day) + '/' + str(year))
+# input('Is this correct? (y/N) ')
+# input()
+
+date = input("Enter date in YYYY-MM-DD format\n")
+year, month, day = map(int, date.split('-'))
+date = datetime.date(year, month, day)
+print()
+print("final date: ", date)
+print("year: ", year)
+print("month:", month)
+print()
+print("------------")
+print()
+
+today = date.today()
+print("today: ", today)
