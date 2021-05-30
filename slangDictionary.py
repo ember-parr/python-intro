e508a470cbc2aa5432ca64c446c0115aa6768d60
Ember P <ember@emberparr.com>
2021-05-30 17:06:52 -0500

    dictionary created for list exploration

---
+++ slangDictionary.py
@@ -0,0 +1,29 @@
+webSlang = ["LOL", "WYD", "IMHO", "IDK"]
+
+print("Choose an option to get started: ")
+print("--- Enter SEARCH to search for a word in the dictionary")
+print("--- Enter ADD to add a new word to the dictionary")
+print("--- Enter EXIT to exit the dictionary")
+app_start = input().lower()
+
+if app_start == "search":
+    user_addition = input("What word would you like to add?\n")
+    if user_addition.upper() in webSlang:
+        print(user_addition + " is in the dictionary!")
+    else:
+        print(user_addition.capitalize() + " is not found.")
+        user_addition_addNew = input("Would you like to add " + user_addition.capitalize() + " to the dictionary? (y/n) - ")
+        if user_addition_addNew == "n":
+            print("no problem... bye!")
+        else: 
+            webSlang.append(user_addition.upper())
+            print("You added a word to the dictonary!")
+            print()
+            print("updated webSlang list:")
+            print(webSlang)
+            print()
+elif app_start == "add":
+    print("not yet an option... ")
+    print()
+else:
+    input("press any key to exit")
