e943b278ae38771fccc8c8817767957b3d91e4e7
Ember P <ember@emberparr.com>
2021-05-30 18:04:22 -0500

    minor tweaks to tax file

--- tax.py
+++ tax.py
@@ -1,3 +1,4 @@
+# hard coded tax rate
 tax = 0.06
 
 def _subtotal(arr):
@@ -5,7 +6,7 @@ def _subtotal(arr):
     for i in arr: 
         subtotal = subtotal + i
     return(subtotal)
-purchases = []
+
 purchases = [48, 18.4, 9.32]
 n = len(purchases)
 subtotal = sum(purchases)
@@ -13,9 +14,7 @@ subtotal = sum(purchases)
 taxDue = subtotal * tax
 totalDue = subtotal + taxDue
 
-name = input("What is your first name?\n")
-
-print('Hey, ' + name + '! Your subtotal is: ', subtotal)
+print('Your subtotal is: ', subtotal)
 print('-- Tax: ', round(taxDue, 2))
 print('Total Due: $', round(totalDue, 2))
 
