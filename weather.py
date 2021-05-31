3bb0eae56a95ed9e0c591a6dbbf72431b7642e18
Ember P <ember@emberparr.com>
2021-05-31 01:00:32 -0500

    added catch for unknown city

--- weather.py
+++ weather.py
@@ -1,14 +1,39 @@
-temperature = int(input("What is today's temperature? "))
+import requests
+import sys
+apiKey = "081c7a4b3bbcea661c0799891be87f17"
 
-if temperature >= 80:
-    print("it's too hot... ")
-    print("let's just not go outside.")
-elif temperature <= 30:
-    print("it's kiiiiiinda cold.")
-    print("cuddle up inside today, mmkay?")
-else :
-    print("okay... you're allowed outside")
+city = input("What city or zip code would you like to check the weather for?\n")
+url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units=imperial"
+request = requests.get(url)
+json = request.json()
 
-print()
-print("press any key to continue")
-input()
+# print(json)
+
+message = json.get("message")
+if message:
+    print("city not found -- exiting program")
+    sys.exit
+else:
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
+
+
+
+    print("Today's forecast in "+city+": "+description)
+    print("   High: ",temp_max,"  ||  ","Low: ",temp_max)
+
+# temperature = int(input("What is today's temperature? "))
+
+# if temperature >= 80:
+#     print("it's too hot... ")
+#     print("let's just not go outside.")
+# elif temperature <= 30:
+#     print("it's kiiiiiinda cold.")
+#     print("cuddle up inside today, mmkay?")
+# else :
+#     print("okay... you're allowed outside")
+
+# print()
+# print("press any key to continue")
+# input()
