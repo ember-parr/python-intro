2348a2eee3ea2871c2a829344bb63f8d62b3b152
Ember P <ember@emberparr.com>
2021-05-30 20:00:25 -0500

    added showtimes to movie menu

--- movieSchedule.py
+++ movieSchedule.py
@@ -1,22 +1,39 @@
 import sys
 
 current_movies = {
-    'The Grinch': '11:00am',
-    'Hardball': '1:00pm'
+    'The Grinch': ['11:00am', "2:00pm", "4:15pm", "7:45pm"],
+    'Hardball': ['11:00am', "2:00pm", "6:15pm", "7:45pm"],
+    'Sharknado': ['9:00am', "12:00pm", "4:30pm", "7:45pm"],
+    'Sharkboy and Lavagirl': ['10:20am', "2:00pm", "4:15pm", "7:45pm"]
 }
 
 
+print("Please choose an option:")
+print("enter [t] to see all movie titles currently playing")
+print("enter [a] to view all movie titles and their showtimes")
+print("enter [s] to search for a movie title")
+menuChoice = input().lower()
 
-menuChoice = input("Would you like to see a list of current movies? (y/n) \n").lower()
-if menuChoice == "y":
+
+
+
+if menuChoice == "t":
     print("\033c")
     print("Current movies available: ")
     for movie in current_movies:
         print("  ", movie)
     print("------")
     user_movieInput = input("What movie would you like showtimes for? \n")
-elif menuChoice == "n":
+elif menuChoice == "s":
     user_movieInput = input("What movie would you like showtimes for? \n")
+elif menuChoice == "a":
+    print("\033c")
+    print("Current movie showtimes: ")
+    for movie in current_movies:
+        print("  ", movie)
+        for time in current_movies.get(movie):
+            print("   -- ", "playing at: ", time)
+        print()
 else:
     print("invalid entry -- exiting program")
     sys.exit()
@@ -27,8 +44,10 @@ showtime = current_movies.get(user_movieInput)
 print()
 if user_movieInput in current_movies:
     if showtime == None:
-        print(user_movieInput, "is not playing today", showtime)
+        print(user_movieInput, "is not playing today")
     else:
-        print(user_movieInput, "is playing today at", showtime)
+        print(user_movieInput, "is playing today at:")
+        for time in showtime:
+            print("   ", time)
 else:
     print("movie not found", user_movieInput)
