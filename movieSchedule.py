b154790fce2d66f9df0f8ecbb9ca00eab44fe990
Ember P <ember@emberparr.com>
2021-05-30 19:35:09 -0500

    added system exit on invalid entry

--- movieSchedule.py
+++ movieSchedule.py
@@ -1,13 +1,24 @@
+import sys
+
 current_movies = {
     'The Grinch': '11:00am',
     'Hardball': '1:00pm'
 }
 
+
+
 menuChoice = input("Would you like to see a list of current movies? (y/n) \n").lower()
 if menuChoice == "y":
+    print("\033c")
+    print("Current movies available: ")
     for movie in current_movies:
-        print(movie)
+        print("  ", movie)
+    print("------")
+    user_movieChoice = input("What movie would you like showtimes for? \n")
 elif menuChoice == "n":
     user_movieChoice = input("What movie would you like showtimes for? \n")
 else:
-    print("invalid input... program closing")
+    print("invalid entry -- exiting program")
+    sys.exit()
+
+print("still goin... ")
