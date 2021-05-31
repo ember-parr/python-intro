ca552c347c0ca7ae83c6d37552bfaf5daa8d65d3
Ember P <ember@emberparr.com>
2021-05-30 19:49:31 -0500

    made user input searchable regarldess of case formatting

--- movieSchedule.py
+++ movieSchedule.py
@@ -14,11 +14,21 @@ if menuChoice == "y":
     for movie in current_movies:
         print("  ", movie)
     print("------")
-    user_movieChoice = input("What movie would you like showtimes for? \n")
+    user_movieInput = input("What movie would you like showtimes for? \n")
 elif menuChoice == "n":
-    user_movieChoice = input("What movie would you like showtimes for? \n")
+    user_movieInput = input("What movie would you like showtimes for? \n")
 else:
     print("invalid entry -- exiting program")
     sys.exit()
+    
 
-print("still goin... ")
+user_movieInput = user_movieInput.title()
+showtime = current_movies.get(user_movieInput)
+print()
+if user_movieInput in current_movies:
+    if showtime == None:
+        print(user_movieInput, "is not playing today", showtime)
+    else:
+        print(user_movieInput, "is playing today at", showtime)
+else:
+    print("movie not found", user_movieInput)
